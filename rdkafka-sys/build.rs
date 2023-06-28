ea80f5c4fb01bba4aa32851f73f0870954ef8c0d
Arthur Silva <arthurprs@gmail.com>
2023-06-28 12:12:59 +0200

    Upgrade rdkafka and fix build

--- rdkafka-sys/build.rs
+++ rdkafka-sys/build.rs
@@ -139,6 +139,7 @@ fn build_librdkafka() {
             cflags.push(format!("-I{}/include", curl_root));
         }
     } else {
+        // There's also no disable-curl????
         configure_flags.push("--disable-curl".into());
     }
 
