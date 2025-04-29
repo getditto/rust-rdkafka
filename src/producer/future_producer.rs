6393b7a9b305583fb176beb6c0fe1617ec0185af
Russell Brown <russell@wombat.me>
2025-04-29 12:56:10 +0100

    WIP: fix build

--- src/producer/future_producer.rs
+++ src/producer/future_producer.rs
@@ -420,7 +420,11 @@ where
                         base_record = record;
                         if !logged {
                             logged = true;
-                            self.context().log(RDKafkaLogLevel::Warning, "FutrueProducer::send_batch", "QueueFull");
+                            self.context().log(
+                                RDKafkaLogLevel::Warning,
+                                "FutrueProducer::send_batch",
+                                "QueueFull",
+                            );
                         }
                         R::delay_for(Duration::from_millis(1)).await;
                     }
@@ -465,7 +469,7 @@ where
         let mut results = Vec::with_capacity(rxs.len() + enqueue_err.is_some() as usize);
         for rx in rxs {
             match rx.await.expect("producer unexpectedly dropped") {
-                Ok((p, o)) => results.push(Ok((p, o))),
+                Ok(d) => results.push(Ok(d)),
                 Err((e, om)) => {
                     results.push(Err((e, om)));
                     break;
